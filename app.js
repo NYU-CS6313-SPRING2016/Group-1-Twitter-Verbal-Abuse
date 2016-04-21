f6fbae958d3fbf32cba188e5af9e40a9fe6a74f0
flyingmeat <wuheng0316@gmail.com>
2016-04-21 12:22:25 -0400

    ++

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var consume = require('./consume.js');
 
 app.engine('.html', require('ejs').__express);
 
-app.configure(function() {
+
     app.set( 'x-powered-by', false);
     app.set( 'views', __dirname + '/views' );
     app.set( 'view engine', 'html' );
     app.use( express.static( path.join( __dirname, 'public' ) ) );
-});  
+  
 
 process.on('uncaughtException', function(err) {
   console.log(err);
